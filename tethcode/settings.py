8137ba054ea9185e93da3e7c37bc1546b79d8b2d
Emmanuel Yakubu <tethcode@gmail.com>
2025-04-28 10:20:04 +0100

    Update settings.py

--- tethcode/settings.py
+++ tethcode/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # The absolute filesystem path to the directory where media files will be stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
